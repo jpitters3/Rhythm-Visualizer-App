487dcd3d232871875de77d0c5a8c927d426247b0
jpitters3 <justin_pitters@yahoo.ca>
2026-01-22 18:15:40 -0500

    Smoothen and even pulse animation

--- js/noteplayer.js
+++ js/noteplayer.js
@@ -139,7 +139,7 @@ let audioCtx = null;
 let audioUnlocked = false;
 let samplesPreloaded = false;
 
-const AUDIO_DELAY = 0.35; // 350ms delay to sync audio with visual pulse expansion
+const AUDIO_DELAY = 0.3; // 300ms delay to sync audio with visual pulse expansion
 
 function unlockAudio() {
   audioUnlocked = true;
