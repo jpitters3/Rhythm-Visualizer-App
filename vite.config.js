e86d4bdaafe0dd7f068f5a72df2c9c24bbd8cf73
jpitters3 <justin_pitters@yahoo.ca>
2026-02-11 20:11:56 -0500

    Configure GitHub Pages for vite project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Rhythm-Visualizer-App/',
   server: {
     port: 3000,
     open: false, // Disable auto-open so it doesn't conflict with VS Code debugger
