85ad30d1ffe7dbd867cbdb298ab811eff60d2e15
jpitters3 <justin_pitters@yahoo.ca>
2026-03-13 17:15:12 -0400

    Move transport controls styles to its own css

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ import './css/games.css';
 import './css/calibration.css';
 import './css/compose-wizard.css';
 import './css/glossary.css';
+import './css/transport-controls.css';
 
 // Import JS (Ordered Dependencies)
 // Using raw imports for side-effect scripts that populate 'window'
